d8767c30d4928369c3d3f9d7980434b9ff73bdeb
pengkun lau <pengkunliu@gmail.com>
2017-09-30 00:11:32 +0800

    add a entity with ref

---
+++ models/news.js
@@ -0,0 +1,26 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var newsSchema = new Schema({
+  type:  {type:Schema.Types.ObjectId, index:true, ref: 'NewsType'},
+  title: {type:String, index:true},
+  link:   String,
+  content: String,
+  coverImg: String,
+  desc: String,
+  key: String,
+  outLink: String,
+  isPublished: {type: String , enum: ['ready', 'published', 'nopublish'], default: 'nopublish'},
+  createDate : { type: Date, default: Date.now },
+  updateDate: { type: Date, default: Date.now }
+});
+
+
+//control columns that should be displayed by different operations
+newsSchema.listColumns = ['_id', 'type', 'title', 'link', 'coverImg', 'desc', 'key', 'outLink', 'isPublished',
+                           'createDate', 'updateDate']
+newsSchema.selectColumns = ['_id', 'type', 'title', 'link', 'content', 'coverImg', 'desc', 'key', 'outLink', 'isPublished',
+                           'createDate', 'updateDate']
+                           
+//module.exports = mongoose.model('Dept', deptSchema);
+module.exports = newsSchema
